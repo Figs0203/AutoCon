d04f280928b7b95cd706e0c09b4be6b935f98a91
sgallo <sofiagdelarosa@gmail.com>
2026-03-20 10:54:19 -0500

    Agregar formulario instancia

--- autocon-server/formats/admin.py
+++ autocon-server/formats/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import FormatoTecnico
+from .models import FormatoTecnico, FormularioInstancia
 
 admin.site.register(FormatoTecnico)
+admin.site.register(FormularioInstancia)
